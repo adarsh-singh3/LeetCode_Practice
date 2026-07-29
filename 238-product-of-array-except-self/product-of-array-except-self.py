db51cd1e65d1d08b68eaf6968001b89799f75d5b
Adarsh Singh <131142298+rockyounow34@users.noreply.github.com>
2026-07-29 09:01:07 +0530

    Time: 24 ms (47.81%) | Memory: 25.2 MB (89.38%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,13 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n  = len(nums)
-        pre = [1]*(n+1)
-        suff = [1]*(n+1)
+        # pre = [1]*(n+1)
+        # suff = [1]*(n+1)
 
-        for i in range(n):
-            pre[i+1] = pre[i]*nums[i]
+        # for i in range(n):
+        #     pre[i+1] = pre[i]*nums[i]
         
+        # for i in reversed(range(n)):
+        #     suff[i] = suff[i+1]*nums[i]
+        # return [pre[i]*suff[i+1] for i in range(n)]
+        ans = [1]*n
+        for i in range(1,n):
+            ans[i] = ans[i-1]*nums[i-1]
+        suff = 1
         for i in reversed(range(n)):
-            suff[i] = suff[i+1]*nums[i]
-        return [pre[i]*suff[i+1] for i in range(n)]
+            ans[i] = ans[i]*suff
+            suff = suff * nums[i]
+        return ans
+        
         
