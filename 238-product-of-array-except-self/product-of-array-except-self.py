1a55a5340fda160c2030d0a661d61e46c7da1edc
adarsh-singh3 <131142298+adarsh-singh3@users.noreply.github.com>
2026-08-30 10:43:24 +0530

    Time: 19 ms (73.14%) | Memory: 25.2 MB (85.56%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -11,19 +11,31 @@ class Solution:
         #         csum = csum*nums[j]
         #     ans.append(csum)
         # return ans
-        pre = [1]*(n+1)
-        for i in range(n):
-            pre[i+1] = pre[i]*nums[i]
-
-        suff = [1]*(n+1)
-        for i in reversed(range(n)):
-            suff[i] = suff[i+1]*nums[i]
         
-        ans = []
+        # pre = [1]*(n+1)
+        # for i in range(n):
+        #     pre[i+1] = pre[i]*nums[i]
 
-        for i in range(n):
-            ans.append(pre[i]*suff[i+1])
+        # suff = [1]*(n+1)
+        # for i in reversed(range(n)):
+        #     suff[i] = suff[i+1]*nums[i]
+        
+        # ans = []
+
+        # for i in range(n):
+        #     ans.append(pre[i]*suff[i+1])
+        # return ans
+
+        ans = [1]*(n)
+        for  i in range(1,n):
+            ans[i] = ans[i-1]*nums[i-1]
+        suff = 1
+        for i in reversed(range(n)):
+            ans[i]*=suff
+            suff*=nums[i]
         return ans
+
+
         
 
         
