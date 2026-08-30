4c3de3da379373aad09ce9b8d66153366dc2e44b
adarsh-singh3 <131142298+adarsh-singh3@users.noreply.github.com>
2026-08-30 10:35:29 +0530

    Time: 34 ms (26.72%) | Memory: 27.7 MB (20.98%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n  = len(nums)
-        # pre = [1]*(n+1)
-        # suff = [1]*(n+1)
+        # ans = []
+        n = len(nums)
 
         # for i in range(n):
-        #     pre[i+1] = pre[i]*nums[i]
-        
-        # for i in reversed(range(n)):
-        #     suff[i] = suff[i+1]*nums[i]
-        # return [pre[i]*suff[i+1] for i in range(n)]
-        ans = [1]*n
-        for i in range(1,n):
-            ans[i] = ans[i-1]*nums[i-1]
-        suff = 1
+        #     csum = 1
+        #     for j in range(n):
+        #         if i==j:
+        #             continue
+        #         csum = csum*nums[j]
+        #     ans.append(csum)
+        # return ans
+        pre = [1]*(n+1)
+        for i in range(n):
+            pre[i+1] = pre[i]*nums[i]
+
+        suff = [1]*(n+1)
         for i in reversed(range(n)):
-            ans[i] = ans[i]*suff
-            suff = suff * nums[i]
+            suff[i] = suff[i+1]*nums[i]
+        
+        ans = []
+
+        for i in range(n):
+            ans.append(pre[i]*suff[i+1])
         return ans
         
+
         
