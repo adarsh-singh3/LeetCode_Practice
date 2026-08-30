dc6baaded76837ed055f22aefd899221cab094bb
adarsh-singh3 <131142298+adarsh-singh3@users.noreply.github.com>
2026-08-30 09:55:07 +0530

    Time: 35 ms (46.55%) | Memory: 21.9 MB (56.34%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -2,11 +2,13 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         freq = {0:1}
         n = len(nums)
-        csum = c = 0
-        for i in nums:
-            csum+=i
+        csum = 0
+        ans = 0
+        for i in range(n):
+            csum+=nums[i]
             if(csum-k in freq):
-                c+=freq.get(csum-k)
+                ans+=freq.get(csum-k)
             freq[csum] = freq.get(csum,0)+1
-        return c
-            
+        return ans
+
+        
