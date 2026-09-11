ead013cca18e01e357a8c36ddae8083f27a33a54
adarsh-singh3 <131142298+adarsh-singh3@users.noreply.github.com>
2026-09-11 07:38:55 +0530

    Time: 87 ms (52.08%) | Memory: 19.7 MB (52.27%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -17,6 +17,27 @@ class Solution:
         # return ans
 
         #better
+        # n = len(s)
+        # l = r = mx = ans = 0
+        # freq = {}
+        # while(r<n):
+        #     freq[s[r]] = freq.get(s[r],0)+1
+        #     mx = max(mx,freq[s[r]])
+        #     change = (r-l+1)-mx
+        #     while(change>k):
+        #         freq[s[l]]-=1
+        #         if(freq[s[l]]==0):
+        #             freq.pop(s[l])
+        #         mx = 0
+        #         for i in freq:
+        #             mx = max(mx,freq.get(i))
+        #         l+=1
+        #         change = (r-l+1)-mx
+        #     ans = max(ans,r-l+1)
+        #     r+=1
+        # return ans
+
+        #optimal
         n = len(s)
         l = r = mx = ans = 0
         freq = {}
@@ -24,13 +45,10 @@ class Solution:
             freq[s[r]] = freq.get(s[r],0)+1
             mx = max(mx,freq[s[r]])
             change = (r-l+1)-mx
-            while(change>k):
+            if(change>k):
                 freq[s[l]]-=1
                 if(freq[s[l]]==0):
                     freq.pop(s[l])
-                mx = 0
-                for i in freq:
-                    mx = max(mx,freq.get(i))
                 l+=1
                 change = (r-l+1)-mx
             ans = max(ans,r-l+1)
