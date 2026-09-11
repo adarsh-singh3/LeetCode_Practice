e177783eac3133523e627419bee5f6fdf4d0111e
adarsh-singh3 <131142298+adarsh-singh3@users.noreply.github.com>
2026-09-11 08:05:07 +0530

    Time: 30 ms (76.55%) | Memory: 23.2 MB (10.68%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,19 +1,33 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def sub(nums,goal):
-            if(goal<0):
-                return 0
-            csum = msum = 0
-            l = 0
-            n = len(nums)
-            for i in range(n):
-                csum+=nums[i]
-                # msum = goal-csum
-                while(csum>goal):
-                    csum-=nums[l]
-                    l+=1
-                msum+=(i-l+1)
-            return msum
-        return sub(nums,goal)-sub(nums,goal-1)
+        #optimal
+        # def sub(nums,goal):
+        #     if(goal<0):
+        #         return 0
+        #     csum = msum = 0
+        #     l = 0
+        #     n = len(nums)
+        #     for i in range(n):
+        #         csum+=nums[i]
+        #         # msum = goal-csum
+        #         while(csum>goal):
+        #             csum-=nums[l]
+        #             l+=1
+        #         msum+=(i-l+1)
+        #     return msum
+        # return sub(nums,goal)-sub(nums,goal-1)
+
+        #better prefix sum + hashmap
+        n = len(nums)
+        freq = {0:1}
+        ans = 0
+        csum = 0
+        for i in range(n):
+            csum+=nums[i]
+            if(csum-goal in freq):
+                ans+=freq.get(csum-goal)
+            freq[csum] = freq.get(csum,0)+1
+        return ans
+
 
         
